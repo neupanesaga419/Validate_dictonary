e0994f53a742ec1989e47f905a7c7ccb031401ce
Sagar Neupane <cyber.crime252@gmail.com>
2022-10-12 22:17:17 +0545

    Code Refactor with change

--- app.py
+++ app.py
@@ -1,44 +1,49 @@
 valid_rule = {
     "name":{
         "type":str,
-        "max_len":100,
-        "min_len":20
+        "max":100,
+        "min":20
         },
     "age":{
         "type":int,
-        "max_value":50,
-        "min_value":16,
+        "max":50,
+        "min":16,
     }
 }
 
-data_dict = {"name":"sagar Neupane ","age":45}
+data_dict = {"name":"sagar Neupane","age":17}
 
 
-def validate_string(key,data_dict, valid_rule):
+def validate_max_min(key,data_dict, valid_rule):
+    if type(data_dict[key]) == str:
+        data = len(data_dict[key])
 
-    if valid_rule[key].get("min_len") and valid_rule[key].get("max_len"):
-        return valid_rule[key]["min_len"] < len(data_dict[key]) < valid_rule[key]["max_len"]
+    if type(data_dict[key]) == int:
+        data = data_dict[key]
+
+    if valid_rule[key].get("min") and valid_rule[key].get("max"):
+        return valid_rule[key]["min"] < data < valid_rule[key]["max"]
     
-    elif valid_rule[key].get("min_len"):
-        return  valid_rule[key]["min_len"] < len(data_dict[key])
+    elif valid_rule[key].get("min"):
+        return  valid_rule[key]["min"] < data
 
-    elif valid_rule[key].get("max_len"):
-        return  valid_rule[key]["max_len"] > len(data_dict[key])
+    elif valid_rule[key].get("max"):
+        return  valid_rule[key]["max"] > data
 
     else:
         return True
 
-def validate_int(key,data_dict, valid_rule):
-    if valid_rule[key].get("max_value") and valid_rule[key].get("min_value"):
-        return valid_rule[key]["min_value"] < data_dict[key] < valid_rule[key]["max_value"]
+# def validate_int(key,data_dict, valid_rule):
+#     if valid_rule[key].get("max_value") and valid_rule[key].get("min_value"):
+#         return valid_rule[key]["min_value"] < data_dict[key] < valid_rule[key]["max_value"]
     
-    elif valid_rule[key].get("max_value"):
-        return valid_rule[key]["max_value"] < data_dict[key]
+#     elif valid_rule[key].get("max_value"):
+#         return valid_rule[key]["max_value"] < data_dict[key]
 
-    elif valid_rule[key].get("max_value"):
-        return valid_rule[key]["max_value"] > data_dict[key]
-    else:
-        return True
+#     elif valid_rule[key].get("max_value"):
+#         return valid_rule[key]["max_value"] > data_dict[key]
+#     else:
+#         return True
 
 
 def validate_dict(data_dict, valid_rule):
@@ -47,11 +52,7 @@ def validate_dict(data_dict, valid_rule):
     for key in valid_rule:
         if data_dict.get(key):
             if valid_rule[key]["type"] == type(data_dict[key]):
-                if type(data_dict[key]) == str:
-                    valid = valid and validate_string(key,data_dict, valid_rule)
-                
-                if type(data_dict[key]) == int:
-                    valid = valid and validate_int(key,data_dict, valid_rule)
+                valid = valid and validate_max_min(key, data_dict, valid_rule)
         
         else:
             valid = False
